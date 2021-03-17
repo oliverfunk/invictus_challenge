a90357dd10f4a7f134dcff745ae6db9387e4c3bb
Oliver Funk <oli.funk@gmail.com>
2021-03-17 15:07:09 +0200

    added the square_odds rpc function

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+from typing import List
 from nameko.rpc import rpc
 
-class GreetingService:
-    name = "greeting_service"
+class InvictusTestService:
+    name = "invt_service"
 
     @rpc
-    def hello(self, name):
-        return "Hello, {}!".format(name)
+    def square_odds(self, in_list: List) -> List:
+        return list(map(lambda n: n if n%2 == 0 else n**2, in_list))
